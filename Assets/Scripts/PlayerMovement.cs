8173ce4234de05bb72980e4462d44689acb4d3f4
Andrea Facchinetti <facchiandrea1510@gmail.com>
2022-11-14 14:15:04 +0100

    Set-up dungeon tiles and basic movement test

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float movementSpeed = 2f;
+    float speedLimiter = 0.75f;
+    float horizontalMovement = 0f;
+    float verticalMovement = 0f;
+    private SpriteRenderer spriteRenderer;
+    private bool movementInputPressed;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            movementInputPressed = true;
+        }
+        else
+        {
+            movementInputPressed = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+
+        if (horizontalMovement != 0 && verticalMovement != 0)
+        {
+            horizontalMovement *= speedLimiter;
+            verticalMovement *= speedLimiter;
+        }
+
+
+        rb.velocity = new Vector2(horizontalMovement * movementSpeed, verticalMovement * movementSpeed);
+
+        if (movementInputPressed == false)
+        {
+            rb.velocity = new Vector2(0, 0);
+
+        }
+
+
+        if (horizontalMovement < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        else if (horizontalMovement > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+
+    }
+}
