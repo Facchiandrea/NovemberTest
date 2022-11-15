aec6928045c5d15f57d5c4ad98ae97de641e66cd
Andrea Facchinetti <facchiandrea1510@gmail.com>
2022-11-15 15:27:42 +0100

    Warrior WIP

---
+++ Assets/Scripts/WarriorScripts/Warrior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : MonoBehaviour
+{
+    public float movementSpeed = 4f;
+
+    public float oldPosition;
+
+    private void Awake()
+    {
+        oldPosition = this.gameObject.transform.position.x;
+    }
+    void Start()
+    {
+    }
+    private void Update()
+    {
+
+        if (transform.position.x > oldPosition)
+        {
+            this.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (transform.position.x < oldPosition)
+        {
+            this.gameObject.transform.localScale = new Vector3(1, 1, 1);
+
+        }
+    }
+    public void FixedUpdate()
+    {
+    }
+
+    private void LateUpdate()
+    {
+        oldPosition = transform.position.x;
+    }
+}
