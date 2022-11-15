82698a4340822a0aaae81bc4290368ba49edaf2a
Andrea Facchinetti <facchiandrea1510@gmail.com>
2022-11-15 21:36:50 +0100

    Key items (no door)

---
+++ Assets/Scripts/PlayerScripts/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Inventory : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    private int keyQuantity;
+    public void ObtainKey()
+    {
+        keyQuantity += 1;
+        text.text = keyQuantity.ToString("00");
+    }
+
+    public void UseKey()
+    {
+        keyQuantity -= 1;
+        text.text = keyQuantity.ToString("00");
+    }
+}
